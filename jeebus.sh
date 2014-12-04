0d4262b56dd60891a836eeee5f484dc595ea04c7
hebda <phil.hebda@gmail.com>
2014-12-04 01:37:14 +0100

    2D running

--- jeebus.sh
+++ jeebus.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 
+#for mgg
 root -l -b -q "Make.C(1,0)"
 root -l -b -q "Make.C(1,1)"
+
+#for mggjj
 #root -l -b -q "Make.C(0,1)"
 #root -l -b -q "Make.C(0,2)"
 
+#for 2D
 #root -l -b -q "Make.C(2,1)"
 #root -l -b -q "Make.C(2,2)"
