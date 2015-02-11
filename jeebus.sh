059ad1b10db9f5744ec29bc921c58bf95e455527
hebda <phil.hebda@gmail.com>
2015-02-11 22:59:45 +0100

    small change

--- jeebus.sh
+++ jeebus.sh
@@ -11,6 +11,6 @@
 
 #for 2D
 root -l -b -q "Make.C(2,0)"
-#root -l -b -q "Make.C(2,1)"
-#root -l -b -q "Make.C(2,2)"
-#root -l -b -q "Make.C(2,3)"
+root -l -b -q "Make.C(2,1)"
+root -l -b -q "Make.C(2,2)"
+root -l -b -q "Make.C(2,3)"
