c536ed216115fda20baa466d21db511609138e14
hebda <phil.hebda@gmail.com>
2015-02-12 01:54:50 +0100

    loop on categories too

--- run2DBiasStudies.sh
+++ run2DBiasStudies.sh
@@ -2,11 +2,13 @@
 
 cat=0
 
-for i in 0 1 2 3 4; do
-    for j in 0 1 2 3 4; do
+for cat in 0 1 2 3; do
+    for i in 0 1 2 3 4; do
+	for j in 0 1 2 3 4; do
 
-	./R2GGBBBiasStudy_2D.exe $cat $i $j
+	    ./R2GGBBBiasStudy_2D.exe $cat $i $j
 
+	done
     done
 done
 
