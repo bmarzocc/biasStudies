d1ecbfb5dacef70acfb4ab2d8dedae82871bdc0b
hebda <phil.hebda@gmail.com>
2015-02-12 01:46:36 +0100

    plot style

--- R2GGBBBiasStudy_2D.cc
+++ R2GGBBBiasStudy_2D.cc
@@ -65,7 +65,7 @@
 using namespace RooFit;
 using namespace RooStats ;
 
-Int_t NCAT = 2;
+Int_t NCAT = 4;
 TString inDir   = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v42/v42_fitTo2D_nonresSearch_withKinFit/";
 
 void AddBkgData(RooWorkspace*, int);
@@ -75,6 +75,7 @@ void BkgModelBias(RooWorkspace*,int,RooAbsPdf*,RooAbsPdf*,FILE*);
 void SetParamNames(RooWorkspace*);
 void SetConstantParams(const RooArgSet* params);
 Double_t effSigma(TH1 *hist);
+void style();
  
 RooArgSet* defineVariables()
 {
@@ -103,13 +104,15 @@ void runfits(int cat=0, int modelNumMgg=0, int modelNumMjj=0, int inDirNum=0)
   RooAbsPdf *MggBkgTruth=0;
   RooAbsPdf *MjjBkgTruth=0;
 
+  style();
+
   TString card_name("hgghbb_models_Pol_8TeV.rs");
   HLFactory hlf("HLFactory", card_name, false);
   RooWorkspace* w = hlf.GetWs();
   AddBkgData(w,cat);
 
   FILE *fout = fopen("resultsBias2D.txt","a");
-  if(modelNumMgg==0 && modelNumMjj ==0) fprintf(fout,"%s\n\n",inDir.Data());
+  //if(modelNumMgg==0 && modelNumMjj ==0) fprintf(fout,"%s\n\n",inDir.Data());
 
   if(modelNumMgg==2 || modelNumMjj==2) return;//skip Landau, it sucks.
   MggBkgTruth = BkgMggModelFit(w,cat,modelNumMgg); //Ber, Exp, Lan, Lau, Pow
@@ -917,4 +920,67 @@ int main(int argc, const char* argv[]){
   runfits(cat,modelNum1,modelNum2);
 
   return 0;
+}
+
+void style(){
+  TStyle *defaultStyle = new TStyle("defaultStyle","Default Style");
+  defaultStyle->SetOptStat(0000);
+  defaultStyle->SetOptFit(000);
+  defaultStyle->SetPalette(1);
+  /////// pad ////////////
+  defaultStyle->SetPadBorderMode(1);
+  defaultStyle->SetPadBorderSize(1);
+  defaultStyle->SetPadColor(0);
+  defaultStyle->SetPadTopMargin(0.05);
+  defaultStyle->SetPadBottomMargin(0.13);
+  defaultStyle->SetPadLeftMargin(0.13);
+  defaultStyle->SetPadRightMargin(0.02);
+  /////// canvas /////////
+  defaultStyle->SetCanvasBorderMode(0);
+  defaultStyle->SetCanvasColor(0);
+  defaultStyle->SetCanvasDefH(600);
+  defaultStyle->SetCanvasDefW(600);
+  /////// frame //////////
+  defaultStyle->SetFrameBorderMode(0);
+  defaultStyle->SetFrameBorderSize(1);
+  defaultStyle->SetFrameFillColor(0);
+  defaultStyle->SetFrameLineColor(1);
+  /////// label //////////
+  defaultStyle->SetLabelOffset(0.005,"XY");
+  defaultStyle->SetLabelSize(0.05,"XY");
+  defaultStyle->SetLabelFont(42,"XY");
+  /////// title //////////
+  defaultStyle->SetTitleOffset(1.1,"X");
+  defaultStyle->SetTitleSize(0.01,"X");
+  defaultStyle->SetTitleOffset(1.25,"Y");
+  defaultStyle->SetTitleSize(0.05,"Y");
+  defaultStyle->SetTitleFont(42, "XYZ");
+  /////// various ////////
+  defaultStyle->SetNdivisions(505,"Y");
+  defaultStyle->SetLegendBorderSize(0); // For the axis titles:
+
+  defaultStyle->SetTitleColor(1, "XYZ");
+  defaultStyle->SetTitleFont(42, "XYZ");
+  defaultStyle->SetTitleSize(0.06, "XYZ");
+ 
+  // defaultStyle->SetTitleYSize(Float_t size = 0.02);
+  defaultStyle->SetTitleXOffset(0.9);
+  defaultStyle->SetTitleYOffset(1.05);
+  // defaultStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
+
+  // For the axis labels:
+  defaultStyle->SetLabelColor(1, "XYZ");
+  defaultStyle->SetLabelFont(42, "XYZ");
+  defaultStyle->SetLabelOffset(0.007, "XYZ");
+  defaultStyle->SetLabelSize(0.04, "XYZ");
+
+  // For the axis:
+  defaultStyle->SetAxisColor(1, "XYZ");
+  defaultStyle->SetStripDecimals(kTRUE);
+  defaultStyle->SetTickLength(0.03, "XYZ");
+  defaultStyle->SetNdivisions(510, "XYZ");
+  defaultStyle->SetPadTickX(1); // To get tick marks on the opposite side of the frame
+  defaultStyle->SetPadTickY(1);
+  defaultStyle->cd();
+  return;
 }
