dbec82ba8660a736a014a7feda3367af4548b25e
hebda <phil.hebda@gmail.com>
2015-02-21 23:48:44 +0100

    new steering script

--- jeebus.sh
+++ jeebus.sh
@@ -10,7 +10,11 @@
 #root -l -b -q "Make.C(0,2)"
 
 #for 2D
-root -l -b -q "Make.C(2,0)"
-root -l -b -q "Make.C(2,1)"
-root -l -b -q "Make.C(2,2)"
-root -l -b -q "Make.C(2,3)"
+for icat in `echo "0 1 2 3"`; do
+    for imgg in `echo "0 1 4"`; do
+	for imjj in `echo "0 1 4"`; do
+	    echo "Running cat $icat mgg $imgg mjj $imjj"
+	    ./R2GGBBBiasStudy_2D.exe $icat $imgg $imjj > a.txt
+	done
+    done
+done
