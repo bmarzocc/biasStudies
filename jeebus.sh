17d7683c8e5e74200461c67ec6ffb56615fef2ce
hebda <phil.hebda@gmail.com>
2015-02-26 17:18:31 +0100

    if everything is in bg, machine will overload. fixed

--- jeebus.sh
+++ jeebus.sh
@@ -17,9 +17,14 @@ withCorr="0 1"
 catrange_m300="0 1"
 catrange_m0="0 1 2 3"
 
+#temporary settings
+searchMass="0"
+withCorr="0 1"
+catrange_m0="0 1 2 3"
+
 for iMass in `echo ${searchMass}`; do
     for iCorr in `echo ${withCorr}`; do
-	if [ "${iMass}"=="300" ]; then
+	if [ "${iMass}" -eq "300" ]; then
 	    catrange=${catrange_m300}
 	else
 	    catrange=${catrange_m0}
@@ -28,7 +33,13 @@ for iMass in `echo ${searchMass}`; do
 	    for imgg in `echo "0 1 4"`; do
 		for imjj in `echo "0 1 4"`; do
 		    echo "Running cat $icat mgg $imgg mjj $imjj searchMass $iMass withCorr $iCorr"
-		    ./R2GGBBBiasStudy_2D.exe $icat $imgg $imjj $iMass $iCorr > output${icat}${imgg}${imjj}${iMass}${iCorr}.txt &
+		    if [ "$imgg" -eq "4" ] && [ "$imjj" -eq "4" ]; then
+			./R2GGBBBiasStudy_2D.exe $icat $imgg $imjj $iMass $iCorr > output-${icat}-${imgg}-${imjj}-${iMass}-${iCorr}.txt
+			sleep 100
+			rm output*txt
+		    else
+			./R2GGBBBiasStudy_2D.exe $icat $imgg $imjj $iMass $iCorr > output-${icat}-${imgg}-${imjj}-${iMass}-${iCorr}.txt &
+		    fi
 		done
 	    done
 	done
