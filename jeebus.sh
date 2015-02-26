2ebffb0982a0ab1b2db55f6c7f9f089dfb553a64
hebda <phil.hebda@gmail.com>
2015-02-26 09:58:23 +0100

    loop on masses too

--- jeebus.sh
+++ jeebus.sh
@@ -10,23 +10,25 @@
 #root -l -b -q "Make.C(0,2)"
 
 #for 2D
-searchMass=0
-#searchMass=300
+searchMass="0 300"
 
-withCorr="1 0"
+withCorr="0 1"
 
 catrange="0 1"
 if [ "$searchMass" -eq "0" ]; then
     catrange="0 1 2 3"
 fi
 
-for iCorr in `echo ${withCorr}`; do
-    for icat in `echo ${catrange}`; do
-	for imgg in `echo "0 1 4"`; do
-	    for imjj in `echo "0 1 4"`; do
-		echo "Running cat $icat mgg $imgg mjj $imjj searchMass $searchMass withCorr $iCorr"
-		./R2GGBBBiasStudy_2D.exe $icat $imgg $imjj $searchMass $iCorr > a.txt
+for iMass in `echo ${searchMass}`; do
+    for iCorr in `echo ${withCorr}`; do
+	for icat in `echo ${catrange}`; do
+	    for imgg in `echo "0 1 4"`; do
+		for imjj in `echo "0 1 4"`; do
+		    echo "Running cat $icat mgg $imgg mjj $imjj searchMass $iMass withCorr $iCorr"
+		    ./R2GGBBBiasStudy_2D.exe $icat $imgg $imjj $iMass $iCorr > output${icat}${imgg}${imjj}${iMass}${iCorr}.txt
+		done
 	    done
 	done
     done
 done
+
